17148633d7cb830783296e9721077cae633079b9
William Zale <williamzale@gmail.com>
2023-03-27 19:14:12 -0400

    Change append method to add more than one node

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -4,10 +4,28 @@ class LinkedList
     @head = nil
   end
 
+  # method that adds a new node to the end
+  # of the linked list
+  # new nodes must be added after the head node
   def append(data)
-    @head = Node.new(data)
+    
+    if @head == nil
+      @head = Node.new(data)
+    else
+      current_node = @head
+      while current_node.next_node != nil
+        current_node = current_node.next_node
+              require 'pry'; binding.pry
+      end
+      current_node.next_node = Node.new(data)
+    end
     data
   end
+
+  #   @head = Node.new(data)
+  #   head.next_node = Node.new(data)
+  #   data
+  # end
 
   def to_string
     self.head.data
